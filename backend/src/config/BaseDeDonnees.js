6e01b95441375f63134ad1d7a166718a50c5258a
OnlyMD-321 <mouadom2003@gmail.com>
2026-06-05 20:40:13 +0100

    vercel deploy

--- backend/src/config/BaseDeDonnees.js
+++ backend/src/config/BaseDeDonnees.js
@@ -50,7 +50,7 @@ class BaseDeDonnees {
             rejectUnauthorized: false,
           },
         },
-        pool: { max: 10, min: 2, acquire: 30000, idle: 10000 },
+        pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
       });
     } else {
       /** Connexion via variables individuelles (Docker Compose, local) */
